0188ec796de49016fd616b1597fa40ea2fe6d77d
contreras-jorgev <contreras.jorgev@gmail.com>
2020-11-04 14:56:08 -0600

    Add project files.

---
+++ CustomerAppSite/README.md
@@ -0,0 +1,31 @@
+﻿# IronSite
+This Sample Project depends on the following ASNA-RND github project
+
+[ASNA Expo Support](https://github.com/asnarnd/QSys)
+
+## After Cloning this Repository, please:
+
+1. Clone the QSys/Expo source.
+2. Update ..\IronSite\libman.json file, with your local references for:
+
+```
+[7]  "library": "../../source/asna/Next/QSys/src/Expo/ExpoTags/src/WebsiteResources/css"
+[15] "library": "../../source/asna/Next/QSys/src/Expo/ExpoTags/src/WebsiteResources/js"
+
+3. If the references are succesful, you will see the wwwroot file structure updated, such that:
+
+```
+wwwroot       
+
+        .  
+        .  
+        .  
+            lib
+                 asna  
+                    css  
+                    js  
+
+                 
+
+
+
---
+++ CustomerAppSite/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ASNA.QSys.ExpoModel;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace CustomerAppSite
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        void ConfigureMonaLisa(IServiceCollection services)
+        {
+            MonaLisaServer monaLisaServer = new MonaLisaServer();
+            Configuration.GetSection("MonaLisaServer").Bind(monaLisaServer);
+
+            if (monaLisaServer.InProcess &&
+                string.Compare(monaLisaServer.HostName, "*LoopBack", true) == 0)
+            {
+                monaLisaServer.Port = ASNA.QSys.MonaServer.Server.StartService(monaLisaServer.Port);
+            }
+
+            //services.AddMonaLisa();
+            services.AddSingleton<IMonaLisaServer>(s=> monaLisaServer);
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // AJAX will need to read Request.Body to be read synchronously  
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            services.ConfigureDisplayPagesOptions(Configuration);
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.IsEssential = true; // make the session cookie Essential -- Problematic for DGPR??
+            });
+
+            // Session data needs a store. AddMemoryCache activates a local memory-based store. 
+            services.AddMemoryCache();
+
+            services.AddRazorPages(razorOptions =>
+            {
+                razorOptions.Conventions.AddAreaPageRoute("IronViews","/CUSTDSPF", "");
+            }).AddMvcOptions (mvcOptions =>
+            {
+                mvcOptions.ValueProviderFactories.Insert(0, new EditedValueProviderFactory());
+            });
+
+            ConfigureMonaLisa(services);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+        }
+    }
+}
